2c812becdc15aa2dfed7b2b6b22f984a118df23f
PedroJacovac <prjacovac@gmail.com>
2022-07-29 16:05:38 -0300

    Criando os arquivos

---
+++ src/edu/pedro/primeirasemana/MinhaClasse.java
@@ -0,0 +1,15 @@
+package edu.pedro.primeirasemana;
+public class MinhaClasse {
+    public static void main(String[] args) {
+        String primeiroNome = "Pedro";
+        String segundoNome = "Jacovac";
+
+        String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
